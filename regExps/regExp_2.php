518afb8acdb6aea24b6e09c25c842dde3d696956
mariya-kos <mariyakosacheva@gmail.com>
2017-05-01 17:16:07 +0300

    regExps_homework

---
+++ regExps/regExp_2.php
@@ -0,0 +1,14 @@
+<?php
+$filename = 'html_to_parse.txt';
+$html = fopen($filename, 'r+');
+$htmlText = fread($html, filesize($filename));
+
+$mask = '/attr__name\">(<span.[^<]*<\/span>)?(?<header>.[^<]*)<\/span><span class="attr__value\">(?<value>.[^<]*)<\/span/imu';
+$htmlText = preg_replace('/\t*\n*/ium', '', $htmlText);
+preg_match_all($mask, $htmlText, $data);
+
+$parsed = '';
+for ($i = 0; $i < count($data['header']); $i++) {
+    $parsed .= $data['header'][$i] . ': ' . $data['value'][$i] . '<br>';
+}
+echo $parsed;
